63c2aee287f7e1e527f95b2eff9380348730194f
simon <Simon.piccot@outlook.fr>
2024-10-25 09:06:24 +0200

    ADD registration

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="">
+    <title>Se connecter</title>
+</head>
+<header>
+    <h1>Se connecter</h1>
+</header>
+<body>
+
+<!-- Formulaire de connexion -->
+<form action="" method="post">
+    <label for="email">Adresse e-mail :</label>
+    <input type="email" id="email" name="email" required><br><br>
+
+    <label for="password">Mot de passe :</label>
+    <input type="password" id="password" name="password" required><br><br>
+
+
+    <input type="submit" value="Se connecter">
+
+
+    <p>Vous n'avez pas encore de compte ? <a href="">Créez un compte ici</a>.</p>
+</form>
+
+</body>
+</html>
