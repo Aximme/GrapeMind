a39036970944fb7ff953bf081145f7a65a0c2718
Lena Charriere <lena.chrr@yahoo.com>
2024-10-25 10:17:04 +0200

    ligne separation
    ligne separation

--- aPropos.php
+++ aPropos.php
@@ -30,6 +30,7 @@ include 'components/header.php';
             <div class="contact-info">
                 <p><strong>Email :</strong> grapemind.upv3@gmail.com</p>
                 <p>Université Paul Valéry, Montpellier 3</p>
+                <hr class="separator">
                 <!-- Google Map Iframe -->
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2887.7452420710115!2d3.867529411068972!3d43.63266105363944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6af217be1ff51%3A0x26d70bd15039af4c!2sUniversit%C3%A9%20Paul-Val%C3%A9ry%20Montpellier%203!5e0!3m2!1sfr!2sfr!4v1729842943368!5m2!1sfr!2sfr" width="100%" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
