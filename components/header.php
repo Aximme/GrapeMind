f1bc350d7601825736dcde438b1b2db088d7e7a4
Lena Charriere <lena.chrr@yahoo.com>
2024-10-25 09:15:51 +0200

    header
    header

--- components/header.php
+++ components/header.php
@@ -1,1 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GrapeMind</title>
+    <link rel="stylesheet" href="../css/main.css">
+</head>
+<body>
+<header class="header">
+    <div class="logo">
+        <img src="../assets/images/image_header.png" alt="GrapeMind Logo">
+        <span class="logo-text">GrapeMind</span>
+    </div>
+    <nav class="navigation">
+        <a href="#">Accueil</a>
+        <a href="#">Carte</a>
+        <a href="#">Événements</a>
+        <a href="#">À propos</a>
+    </nav>
+    <div class="auth">
+        <a href="#" class="login">Se connecter</a>
+        <a href="#" class="signup">S'inscrire</a>
+    </div>
+    <!-- Menu Déroulant -->
+    <div class="menu-icon">
+        <span id="menu-toggle">&#9776;</span>
+    </div>
+    <div id="dropdown-menu" class="dropdown-menu">
+        <a href="#">MON PROFIL</a>
+        <a href="#">STATISTIQUES</a>
+        <a href="#">PARAMÈTRES DU COMPTE</a>
+    </div>
+    <script src="../js/menu_header.js"></script>
+</header>
