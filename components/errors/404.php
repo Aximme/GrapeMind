95fa92d85039bac888e2d361eca38ad68fab2a0f
Aximme <maxime.miroff@icloud.com>
2024-11-10 00:41:05 +0100

    Errors pages
    - gestion for apache & nginx server

---
+++ components/errors/404.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page non trouvée - Erreur 404</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f8f9fa;
+            text-align: center;
+            padding: 50px;
+        }
+        h1 {
+            font-size: 50px;
+            color: #721c24;
+        }
+        p {
+            font-size: 20px;
+            color: #6c757d;
+        }
+        a {
+            color: #007bff;
+            text-decoration: none;
+        }
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+<img src="/GrapeMind/assets/images/error.png" alt="Error image">
+<h1>404</h1>
+<p>Oups ! La page que vous cherchez n'existe pas.</p>
+<p><a href="/GrapeMind/index.php">Retour à la page d'accueil</a></p>
+</body>
+</html>
