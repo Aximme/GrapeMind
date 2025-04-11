aacf413f74a7680a5464d4ae1906b048b1d10d61
Aximme <maxime.miroff@icloud.com>
2025-04-11 13:00:27 +0200

    Update save_quiz.php

--- components/quizz/save_quiz.php
+++ components/quizz/save_quiz.php
@@ -3,7 +3,7 @@ session_start();
 global $conn;
 header('Content-Type: application/json');
 
-require_once __DIR__ . '../../db.php';
+require_once __DIR__ . '/../../db.php';
 $user_id = $_SESSION['user']['id'];
 
 
