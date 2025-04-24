ef661754177aac2d5c92a2c121b911dd7f42b82d
Maxime <maxime.miroff@icloud.com>
2025-04-24 23:15:57 +0200

    Create 403.php

---
+++ components/errors/403.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accès Refusé - Erreur 403</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f8f9fa;
+            text-align: center;
+            padding: 50px;
+        }
+        h1 {
+            font-size: 50px;
+            color: #721c24;
+        }
+        p {
+            font-size: 20px;
+            color: #6c757d;
+        }
+        a {
+            color: #007bff;
+            text-decoration: none;
+        }
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+<img src="/assets/images/error.png" alt="Error image">
+<h1>404</h1>
+<p>Visiblement, tu n'as rien a faire la :)</p>
+<p><a href="/index.php">Retour à la page d'accueil</a></p>
+</body>
+</html>
